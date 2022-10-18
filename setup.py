1b134e7b9a2567e0e8f2c94ad2264efb10b3c703
Nick-AI <yannikglaser@gmail.com>
2022-10-17 16:03:59 -1000

    Update pre analytics think tank presentation

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="src",
+    name="demo_src",
     packages=find_packages(),
 )
--- src/training/__init__.py
+++
@@ -1,4 +0,0 @@
-""" Training logic for contrastive and linear readout tasks. 
-
-Code adapted from https://github.com/HobbitLong/PyContrast.
-"""
